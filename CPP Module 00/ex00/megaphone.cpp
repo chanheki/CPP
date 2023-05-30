6facc5d39afe59cfbab6da387ba77e938c542bf1
chanhihi <chanhihi55@gmail.com>
2023-05-30 19:42:15 +0900

    feat(cpp00-ex00): megaphone

---
+++ CPP Module 00/ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by Chan on 2023/05/12.
+//
+
+#include <iostream>
+
+void printUppercase(int argc, char **argv){
+	for (int i = 1; i < argc; i++) {
+		std::string word(argv[i]);
+		for (size_t j = 0; j < word.length(); j++) {
+			char c = std::toupper(word[j]);
+			std::cout << c;
+		}
+	}
+	std::cout << std::endl;
+}
+
+int main(int argc, char **argv) {
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	printUppercase(argc, argv);
+	return 0;
+}
