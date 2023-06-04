dfb7ecddd7086802e296f18eca43e573baf9da23
chanhihi <chanhihi55@gmail.com>
2023-06-05 01:51:03 +0900

    feat(CPP Module 02): submitted repository

--- CPP Module 02/ex03/bsp.hpp
+++ CPP Module 02/ex03/bsp.hpp
@@ -6,16 +6,6 @@
 #include "Fixed.hpp"
 #include "Point.hpp"
 
-class BSP {
-
-private:
-public:
-	BSP(void);
-	BSP(BSP const &src);
-	~BSP(void);
-	BSP &operator=(BSP const &rhs);
-};
-
 bool bsp(Point const a, Point const b, Point const c, Point const point);
 std::string result(Point const a, Point const b, Point const c, Point const point);
 
